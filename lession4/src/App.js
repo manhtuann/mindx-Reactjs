207b15cc4b8ac38854234167e36d29b021dd75c8
manhtuan <pmtss2k1@gmail.com>
2022-06-29 22:34:29 +0700

    lession4

---
+++ lession4/src/App.js
@@ -0,0 +1,18 @@
+
+import { useState} from 'react'
+import  Form  from './components/Form'
+import  Home  from './components/Home'
+
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <Home />
+      <Form />
+    </div>
+  );
+}
+
+export default App;
